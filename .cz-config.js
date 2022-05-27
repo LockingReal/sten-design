470151576c42b0991e1d9149e22508b830079fa7
LockingReal <1191515088@qq.com>
2022-05-28 01:48:13 +0800

    ✨: feat(其它变更):

--- .cz-config.js
+++ .cz-config.js
@@ -1,34 +1,30 @@
+"use strict";
 module.exports = {
-    types: [
-        { value: "feat", name: "feat 🍄:    新增新的特性" },
-        { value: "fix", name: "fix 🐛:    修复 BUG" },
-        { value: "docs", name: "docs 📄:    修改文档、注释" },
-        { value: "refactor", name: "refactor 🎸:    代码重构，注意和特性、修复区分开" },
-        { value: "perf", name: "perf ⚡:    提升性能" },
-        { value: "test", name: "test 👀:    添加一个测试" },
-        { value: "tool", name: "tool 🚗:    开发工具变动(构建、脚手架工具等)" },
-        { value: "style", name: "style ✂:    对代码格式的修改不影响逻辑" },
-        { value: "revert", name: "revert 🌝:     版本回滚" },
-        { value: "update", name: "update ⬆:    第三方库升级 " }
-    ],
-
-    scopes: [{ name: '组件' }, { name: '样式' }, { name: '文档更改' }, { name: '其它变更' }],
-
-    allowTicketNumber: false,
-    isTicketNumberRequired: false,
-    ticketNumberPrefix: 'TICKET-',
-    ticketNumberRegExp: '\d{1,5}',
-    messages: {
-        type: "选择一种你的提交类型:",
-        scope: "选择一个scope (可选):",
-        customScope: "Denote the SCOPE of this change:",
-        subject: "简要说明:\n",
-        body: '详细说明，使用"|"换行(可选)：\n',
-        breaking: "非兼容性说明 (可选):\n",
-        footer: "关联关闭的issue，例如：#31, #34(可选):\n",
-        confirmCommit: "确定提交?"
-    },
-
-    allowCustomScopes: true,
-    allowBreakingChanges: ['新增', '修复'], subjectLimit: 100
+  types: [
+    { value: "✨", name: "特性:    一个新的特性" },
+    { value: "🐛", name: "修复:    修复一个Bug" },
+    { value: "📝", name: "文档:    变更的只有文档" },
+    { value: "💄", name: "格式:    空格, 分号等格式修复" },
+    { value: "♻️", name: "重构:    代码重构，注意和特性、修复区分开" },
+    { value: "⚡️", name: "性能:    提升性能" },
+    { value: "✅", name: "测试:    添加一个测试" },
+    { value: "🔧", name: "工具:    开发工具变动(构建、脚手架工具等)" },
+    { value: "⏪", name: "回滚:    代码回退" },
+  ],
+  scopes: [
+    { name: "component" },
+  ],
+  messages: {
+    type: "选择一种你的提交类型:",
+    scope: "选择一个scope (可选):",
+    customScope: "Denote the SCOPE of this change:",
+    subject: "短说明:\n",
+    body: '长说明，使用"|"换行(可选)：\n',
+    breaking: "非兼容性说明 (可选):\n",
+    footer: "关联关闭的issue，例如：#31, #34(可选):\n",
+    confirmCommit: "确定提交说明?",
+  },
+  allowCustomScopes: true,
+  allowBreakingChanges: ["特性", "修复"],
+  subjectLimit: 100,
 };
